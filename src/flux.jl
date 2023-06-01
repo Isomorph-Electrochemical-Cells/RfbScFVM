34de31365fce9dcda30a167e30a3583bbca8f6a8
Roman Schaerer <r.p.s@posteo.ch>
2023-06-01 15:25:13 +0200

    Add flux function

--- src/flux.jl
+++ src/flux.jl
@@ -6,6 +6,16 @@ function fvc_b(z)
     return z/(exp(z)-one(z))
 end
 
+function fvc_flux_Δx(ul, ur, v, d, Δx)
+    p = v*Δx/d # Cell Peclet number
+
+    # Evaluate Bernoulli function
+    bm = fvc_b(-p)
+    bp = fvc_b(p)
+
+    return d * (bm * ul - bp * ur)
+end
+
 function fvc_flux_Δx(ul, ur, vh, d)
     p = vh/d # Cell Peclet number
 
